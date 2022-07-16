4f581b56b8b9336bdd93bec0385e3c2a9956601a
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2022-07-16 11:56:41 +0100

    Update FileSorts.h

--- es-app/src/FileSorts.h
+++ es-app/src/FileSorts.h
@@ -20,7 +20,7 @@ namespace FileSorts
 	bool compareRegion(const FileData* file1, const FileData* file2);
 	bool compareSystem(const FileData* file1, const FileData* file2);
 
-	void ignoreLeadingArticles(std::string &name1, std::string &name2);
+//	void ignoreLeadingArticles(std::string &name1, std::string &name2);
 
 	extern const std::vector<FileData::SortType> SortTypes;
 };
